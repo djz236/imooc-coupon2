6a259f622be8b3d75e3bcc86be26866736027fe5
djz236 <djz236@163.com>
2021-02-21 20:55:16 +0800

    imooc-coupon2

--- coupon2-eureka/src/main/java/com/imooc/coupon/EurekaApplication.java
+++ coupon2-eureka/src/main/java/com/imooc/coupon/EurekaApplication.java
@@ -1,4 +1,9 @@
 package com.imooc.coupon;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
 }
